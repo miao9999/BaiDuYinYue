22cc41d74eeea5f9f89c6b465cb4528ab5eec8ee
limiao <1374113170@qq.com>
2016-07-11 21:42:39 +0800

    外层的fragment

---
+++ BaiDuYinYue/app/src/main/java/com/example/dllo/baiduyinyue/ui/fragment/MusicFragment.java
@@ -0,0 +1,23 @@
+package com.example.dllo.baiduyinyue.ui.fragment;
+
+import com.example.dllo.baiduyinyue.R;
+
+/**
+ * Created by Limiao on 16/7/11.
+ */
+public class MusicFragment extends AbsBaseFragment {
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_music;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
